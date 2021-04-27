a32f7671d6a390d4ed0a4f79ba935708a603a59e
Nirmal Abraham <nabrah@codeaurora.org>
2021-04-27 13:30:01 +0530

    fbdev: msm: Handle rotator init failure correctly
    In case 'mdss_rotator_req_init' function fails, the
    error handling code frees the 'req' struct which
    is not yet allocated. Correct this error scenario
    by freeing the 'req' struct only for cases where it
    is already allocated.
    
    Change-Id: If9089c59497fef628528e6a462eb5a4699453214
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/fbdev/msm/mdss_rotator.c
+++ drivers/video/fbdev/msm/mdss_rotator.c
@@ -2224,14 +2224,16 @@ static int mdss_rotator_handle_request(struct mdss_rot_mgr *mgr,
 	ret = copy_from_user(items, user_req.list, size);
 	if (ret) {
 		pr_err("fail to copy rotation items\n");
-		goto handle_request_err;
+		kfree(items);
+		return ret;
 	}
 
 	req = mdss_rotator_req_init(mgr, items, user_req.count, user_req.flags);
 	if (IS_ERR_OR_NULL(req)) {
 		pr_err("fail to allocate rotation request\n");
 		ret = PTR_ERR(req);
-		goto handle_request_err;
+		kfree(items);
+		return ret;
 	}
 
 	mutex_lock(&mgr->lock);
@@ -2270,7 +2272,6 @@ static int mdss_rotator_handle_request(struct mdss_rot_mgr *mgr,
 
 handle_request_err1:
 	mutex_unlock(&mgr->lock);
-handle_request_err:
 	kfree(items);
 	kfree(req);
 	return ret;
@@ -2389,7 +2390,8 @@ static int mdss_rotator_handle_request32(struct mdss_rot_mgr *mgr,
 	ret = copy_from_user(items, compat_ptr(user_req32.list), size);
 	if (ret) {
 		pr_err("fail to copy rotation items\n");
-		goto handle_request32_err;
+		kfree(items);
+		return ret;
 	}
 
 	req = mdss_rotator_req_init(mgr, items, user_req32.count,
@@ -2397,7 +2399,8 @@ static int mdss_rotator_handle_request32(struct mdss_rot_mgr *mgr,
 	if (IS_ERR_OR_NULL(req)) {
 		pr_err("fail to allocate rotation request\n");
 		ret = PTR_ERR(req);
-		goto handle_request32_err;
+		kfree(items);
+		return ret;
 	}
 
 	mutex_lock(&mgr->lock);
@@ -2436,7 +2439,6 @@ static int mdss_rotator_handle_request32(struct mdss_rot_mgr *mgr,
 
 handle_request32_err1:
 	mutex_unlock(&mgr->lock);
-handle_request32_err:
 	kfree(items);
 	kfree(req);
 	return ret;
