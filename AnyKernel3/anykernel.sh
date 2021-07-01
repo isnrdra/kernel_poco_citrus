33ce678b4511532176767db9035293ed2bf211ae
Alex Finhart <alexfinhart@gmail.com>
2021-07-01 18:28:37 +0800

    AnyKernel: track changes * will add post-fs scripts on later builds. Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,30 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Chaeyoung Kernel
+maintainer.string=dlwlrm123
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+'; }
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+## end install
+
