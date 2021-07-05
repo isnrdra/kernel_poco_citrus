85116c4b483036cc19834da0788a521347534f94
root <alexfinhart@gmail.com>
2021-07-05 13:39:51 +0800

    mm/page_alloc: add CIB and devfreq_boost trigger
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -69,6 +69,8 @@
 #include <linux/nmi.h>
 #include <linux/khugepaged.h>
 #include <linux/psi.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/sections.h>
 #include <asm/tlbflush.h>
@@ -4684,6 +4686,12 @@ retry:
 	if (costly_order && !(gfp_mask & __GFP_RETRY_MAYFAIL))
 		goto nopage;
 
+	/* Boost when memory is low so allocation latency doesn't get too bad */
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPU_LLCCBW, 100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW_DDR, 100);
+	pr_info_ratelimited("%s: boosting allocation latency\n", __func__);
+	
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
 		goto retry;
